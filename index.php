a026cbb9c28e3c9c0a990b3b537d009f6381b3c9
Colin McCulloch <colinmucculloch@gmail.com>
2020-04-14 14:03:39 -0400

    inital

---
+++ index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>PHP Test Sandbox</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class='col-12 d-flex justify-content-around'>
+        <!-- <p>Hello world this is my php sandbox page.</p> -->
+        <?php
+        // $x = 10;
+        // for ($y = 0; $y <= $x; $y++) {
+        //     echo 'the item number is ' . $y . '<br>';
+        // }
+        $servername = "localhost";
+        $username = "root";
+        $password = "root";
+        $dbname = "example";
+
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        echo "Connected succesfully"."<br>";
+            // $sql = "INSERT INTO example (name) VALUES ('Doe')";
+            // if ($conn->query($sql) === TRUE) {
+            //     echo "New record created successfully".'<br>';
+            // }else {
+            //     echo "Error: " . $sql . "<br>" . $conn->error;
+            // }
+        $sql= "SELECT * FROM example";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "id: " . $row["id"]. " - Name: " . $row["name"] . "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+
+
+
+        ?>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
